1bb4faf01273061c3bfb05454837f0eb25449209
Markov146 <denissmarkofff@yandex.ru>
2023-02-09 01:23:12 +0300

    Загружаю свой проект

---
+++ Prowodnik/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Prowodnik
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.SetCursorPosition(50, 0);
+            Console.WriteLine("Этот компьютер");
+            Console.WriteLine("------------------------------------------------------------------------------------------------------------------------");
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            Console.SetCursorPosition(0, 3);
+            foreach (DriveInfo drive in allDrives)
+            {
+                Console.WriteLine($"  {drive.Name} Свободно {drive.TotalFreeSpace / 1073741824} ГБ из {drive.TotalSize / 1073741824} ГБ");
+
+            }
+        }
+        
+        public void DirConsole(string pathToFolder)
+        {
+            Dir(pathToFolder, 0);
+
+            void Dir(string nameFolder, int level)
+            {
+                string[] allfiles = Directory.GetFiles(nameFolder);
+                foreach (string filename in allfiles)
+                {
+                    Console.WriteLine(new string('\t', level) + filename);
+                }
+                string[] allfolders = Directory.GetDirectories(pathToFolder);
+                foreach (string folder in allfolders)
+                {
+                    Console.WriteLine(new string('\t', level) + folder);
+                    Dir(folder, level + 1);
+                }
+            }          
+                Console.Clear();               
+            
+            
+
+
+
+        }
+
+
+     }
+     
+}
---
+++ Prowodnik/Strelki.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prowodnik
+{
+    internal class Strelki
+    {       
+        
+        public static int Max, Min;
+        public static int position = 3;
+        public Strelki(int min, int max)
+        {
+            
+            Max = max;
+            Min = min;
+        }
+        public int Strelka()
+        {
+            ConsoleKeyInfo key = Console.ReadKey();
+            do
+            {
+                Console.SetCursorPosition(0, position);
+                Console.Write("  ");
+                switch (key.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        position--;
+                      
+                        if (position < Min)
+                        {
+                            position = Max;
+                        }
+                        else if (position > Max + 1)
+                        {
+                            position = Min + 1;
+                        }
+                        Console.SetCursorPosition(0, position);
+                        Console.WriteLine("->");
+                        break;
+                    case ConsoleKey.DownArrow:
+                        position++;
+                        if (position <= Min + 1)
+                        {
+                            position = Max;
+                        }
+                        else if (position > Max + 1)
+                        {
+                            position = Min;
+                        }
+                        Console.SetCursorPosition(0, position);
+                        Console.WriteLine("->");
+                        break;
+                    case ConsoleKey.Escape:
+                        return -1111;
+                        break;
+                }
+            } while (key.Key != ConsoleKey.Enter);
+             Console.Clear();
+             
+        }
+    }
+}
+
